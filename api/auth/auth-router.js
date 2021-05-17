47a43b763f889ca2cae60b38f8c36cb2e9476977
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-17 18:51:42 +0200

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -3,7 +3,9 @@ const User = require('../users/users-model')
 const bcrypt = require('bcryptjs')
 
 router.post('/register', (req, res, next) => {
+  const { username, password } = req.body
   
+
 })
 router.post('/login', (req, res, next) => {
   res.json({ message: 'login' })
