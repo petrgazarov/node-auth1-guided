cb4c9f4823972a5bb450e95dc1f15654911f2f41
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-17 19:30:45 +0200

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -26,11 +26,13 @@ router.post('/login', (req, res, next) => {
   User.findBy({ username })
     .then(([user]) => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        // good
+        res.json({
+          message: `Welcome back, ${user.username}`,
+        })
       } else {
         next({
           status: 401,
-          message: `Invalid credentials`
+          message: `Invalid credentials`,
         })
       }
     })
