a3701f07b05131deb0d84ea822c7fd64d14d0d94
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-17 19:50:27 +0200

    wip

--- api/server.js
+++ api/server.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const express = require('express');
 const helmet = require('helmet');
-const session = require('express-session')
-const 
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 const usersRouter = require('./users/users-router.js');
 const authRouter = require('./auth/auth-router.js');
