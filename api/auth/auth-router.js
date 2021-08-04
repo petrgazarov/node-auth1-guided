bae474cd97eb24013f0558606a9a4dc9fe89bdf0
Petr Gazarov <petrgazarov@gmail.com>
2021-08-03 19:53:47 -0700

    Implement login endpoint

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -4,7 +4,24 @@ const router = require("express").Router();
 const User = require("../users/users-model.js");
 
 router.post('/login', (req, res) => {
-  req.session.something = 123;
+  const { username, password } = req.body;
+
+  // 1. Check if there exists a user with this username
+  // 2. If yes, verify that password matches. If it does, save the session and set the cookie. If it does not, send a 401
+  // 3. If no, send a 401
+
+  User
+    .findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.session.user = user;
+
+        res.json({ message: `Welcome back ${user.username}, have a cookie!` });
+      } else {
+        res.status(401).json({ message: 'Invalid credentials' });
+      }
+    });
   
   res.json({});
 });
