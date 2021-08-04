d3462f8337d3a87fb68b555a635a481c00b6cb54
Petr Gazarov <petrgazarov@gmail.com>
2021-08-03 19:13:19 -0700

    Add session middleware and brypt

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const helmet = require('helmet');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 const usersRouter = require('./users/users-router.js');
 
@@ -9,6 +11,28 @@ const server = express();
 server.use(express.static(path.join(__dirname, '../client')));
 server.use(helmet());
 server.use(express.json());
+
+const sessionConfig = {
+  name: 'monkey',
+  secret: 'keep it secret, keep it safe!',
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false, // if true the cookie is not set unless it's an https connection
+    httpOnly: false, // if true the cookie is not accessible through document.cookie
+  },
+  rolling: true,
+  resave: false, // some data stores need this set to true
+  saveUninitialized: false, // privacy implications, if false no cookie is set on client unless the req.session is changed
+  store: new KnexSessionStore({
+    knex: require('../database/db-config.js'), // configured instance of knex
+    tablename: 'sessions', // table that will store sessions inside the db, name it anything you want
+    sidfieldname: 'sid', // column that will hold the session id, name it anything you want
+    createtable: true, // if the table does not exist, it will create it automatically
+    clearInterval: 1000 * 60 * 60, // time it takes to check for old sessions and remove them from the database to keep it clean and performant
+  }),
+};
+
+server.use(session(sessionConfig));
 
 server.use('/api/users', usersRouter);
 
