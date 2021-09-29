3f1eeced9110f84488f9ea01e649564f788fd0f7
Petr Gazarov <petrgazarov@gmail.com>
2021-09-28 19:26:37 -0700

    Implement login (wip)

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
+const { json } = require('express');
 const router = require('express').Router();
-const { add } = require('../users/users-model');
+const { add, findBy } = require('../users/users-model');
 
 router.post('/register', async (req, res, next) => {
   try {
@@ -12,6 +13,23 @@ router.post('/register', async (req, res, next) => {
 
     res.status(201).json(user);
   } catch(e) {
+    next(e);
+  }
+});
+
+router.post('/login', async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    const [existingUser] = await findBy({ username });
+
+    if (existingUser && bcrypt.compareSync(password, existingUser.password)) {
+      // TODO
+      res.json({ message: 'Success' });
+    } else {
+      res.status(401).json({ message: 'Bad credentials!' });
+    }
+  } catch (e) {
     next(e);
   }
 });
